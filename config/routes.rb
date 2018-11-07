d8a609f268796d3e76446bb8a92d729c94eeeeb3
remka <bolist@mail.ru>
2018-11-07 20:18:57 +0200

    Make static page and index

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'main#index', as: 'home'
+  get 'fotoprint' => 'pages#fotoprint', as: 'fotoprint'
+  get 'action' => 'pages#action', as: 'action'
+  get 'price' => 'pages#price', as:'price'
+  get 'delivery' => 'pages#delivery', as: 'delivery'
+  get 'faq' => 'pages#faq', as:'faq'
+  get 'contacts' => 'pages#contacts', as:'contacts'
+
+  resources :posts
 end
