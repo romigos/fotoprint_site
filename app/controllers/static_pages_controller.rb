390a1379efe796ead643fb4e6402f11f57dba7b2
Roman <bolist@mail.ru>
2018-11-30 13:38:17 +0200

    Make static page and index

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,25 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def services
+  end
+
+  def price
+  end
+
+  def payment
+  end
+
+  def delivery
+  end
+
+  def conditions
+  end
+
+  def faq
+  end
+
+  def contacts
+  end
+end
