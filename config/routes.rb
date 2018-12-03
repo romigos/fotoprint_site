2465e99b876d589b145e9f9fed3d859838bd05ca
remka <bolist@mail.ru>
2018-12-03 08:27:47 +0200

    Make autentification

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-
+  devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
   root  'static_pages#home'
   get 'services' => 'static_pages#services', as:'services'
   get 'price' => 'static_pages#price', as:'price'
